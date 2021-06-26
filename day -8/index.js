84ad5387c979b77db6cd0bd34cb51b13dd72940b
Aman kumar gupta <49440235+amangupta590@users.noreply.github.com>
2021-06-26 12:48:13 +0530

    Update index.js

--- day -8/index.js
+++ day -8/index.js
@@ -1,12 +1,11 @@
-document.querySelector(".main").addEventListener("click",oncl);
-function oncl(e)
+document.querySelector(".main").addEventListener("click",clc);
+function clc(e)
 {
-    var c = e.target.className;
-    if(c == "unclicked"){
-        e.target.classList = "clicked";
+    var val=e.target.className;
+    if(val == "clicked"){
+        e.target.classList= "unclicked";
     }
-    if(c == "clicked"){
-        e.target.classList = "unclicked";
-    } 
- 
+    if(val == "unclicked"){
+        e.target.classList= "clicked";
+    }
 }
